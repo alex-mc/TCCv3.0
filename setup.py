4d7661a2611f670f14964fa012c363d7105e1456
alex-mc <mcmaster2010@gmail.com>
2015-06-22 21:25:30 -0700

    revised setup.py
    unspecified error thrown on distutils.core.Extension() call

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-#! python
+#!python
 
 from distutils.core import setup, Extension
 
-test_module = Extension('_bct30_wrap_compiled', sources=['bct30_wrap.cxx'],)
+test_module = Extension('_bct30_swig',
+                        include_dirs = ['C:/Users/Alex/Google Drive/MRO/TCCv3.0/'],
+                        libraries = ['bct30.h', 'indiapi.h'],
+                        sources=['bct30_wrap.cxx', 'bct30.cpp'],
+                        )
 
-setup (name = 'test_module',
-		version = '0.1',
-		author = "Alex McMaster",
-		)
+print('made it')
+setup (name = 'bct30',
+       version = '0.1',
+       author = "Alex McMaster",
+       description = """A test module""",
+       ext_modules = [test_module],
+       py_modules = ['bct30_swig'],
+       )
